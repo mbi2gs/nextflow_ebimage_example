5da979b2f245147462cb61245ff70a488e246db4
mbi2gs <mattbbiggs@gmail.com>
2018-11-01 22:09:00 -0400

    initial commit

---
+++ bin/ia.R
@@ -0,0 +1,16 @@
+library(EBImage)
+
+args = commandArgs(trailingOnly = TRUE)
+
+c2 = readImage(args[1])
+
+c2.b = c2*2
+
+c2.b.blur = gblur(c2.b, sigma = 5)
+
+c2.b.blur.thres = c2.b.blur > otsu(c2.b.blur)
+
+c2.b.blur.thres.cnt = bwlabel(c2.b.blur.thres)
+
+nucNo = max(bwlabel(c2.b.blur.thres))
+print(nucNo)
