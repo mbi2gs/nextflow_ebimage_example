66a452b1b47c1ec679dd315b140a292c93fa28f4
mbi2gs <mattbbiggs@gmail.com>
2018-11-02 21:57:54 -0400

    removed nextflow files

--- work/68/16a38c300898d505b2d3b95530898c/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript /home/mbbiggs/nextflow_ebimage_example/bin/ia.R Dros_c1.tif?raw=true > out.csv
--- work/f0/b1a096ff08e5e104031ad2465904d9/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript /home/mbbiggs/nextflow_ebimage_example/bin/ia.R Dros_c3.tif > out.csv
