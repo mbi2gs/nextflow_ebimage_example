3d7dbfe50f894de9464fc120b9807c9d67829200
Matt Biggs <mb3ad@virginia.edu>
2018-11-02 22:35:43 -0400

    updated ia.R

--- bin/ia.R
+++ bin/ia.R
@@ -13,4 +13,8 @@ c2.b.blur.thres = c2.b.blur > otsu(c2.b.blur)
 c2.b.blur.thres.cnt = bwlabel(c2.b.blur.thres)
 
 nucNo = max(bwlabel(c2.b.blur.thres))
-print(paste(args[1],nucNo))
+print(paste(args[1],nucNo), sep = ',')
+
+fileConn = file("out.csv")
+writeLines(paste(args[1], nucNo, sep = ','), fileConn)
+close(fileConn)
