34b15af8e1ecfda70b5d702ad2b62f234dfc07ea
Matt Biggs <mb3ad@virginia.edu>
2018-11-02 22:28:14 -0400

    Update bin/ia.R

--- bin/ia.R
+++ bin/ia.R
@@ -13,4 +13,4 @@ c2.b.blur.thres = c2.b.blur > otsu(c2.b.blur)
 c2.b.blur.thres.cnt = bwlabel(c2.b.blur.thres)
 
 nucNo = max(bwlabel(c2.b.blur.thres))
-print(paste(args[1],nucNo))`
+print(paste(args[1],nucNo))
